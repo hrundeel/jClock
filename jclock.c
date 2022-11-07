f9c6d29ea8d2e060f95224811b5fd738e85e59da
hrundeel <69468668+hrundeel@users.noreply.github.com>
2022-11-07 21:23:03 +0400

    fix links.

--- jclock.c
+++ jclock.c
@@ -1,15 +1,15 @@
 /*
 * jClock - basic clock with clock synchronization protocol JJY
-* Author: hrundeel (https://gist.github.com/hrundeel)
+* Author: hrundeel (https://github.com/hrundeel)
 * Added:
 *   Options for JJY disable/enable.
 *   Options for JJY delta TZ (if your current watch region don't match flipper time).
 *   Reworked getting rtc time/update local values.
 *   Reworked main screen (+charge/jjy/tx status icons, +charge%%).
 * Based on:
-*   Simple clock for Flipper Zero by CompaqDisc (https://gist.github.com/CompaqDisc, https://gist.github.com/CompaqDisc/4e329c501bd03c1e801849b81f48ea61)
+*   Simple clock for Flipper Zero by CompaqDisc (https://github.com/CompaqDisc, https://gist.github.com/CompaqDisc/4e329c501bd03c1e801849b81f48ea61)
 *   Timer by GMMan (?)
-*   Settings by kowalski7cc (https://gist.github.com/kowalski7cc)
+*   Settings by kowalski7cc (https://github.com/kowalski7cc)
 */
 
 #include <furi.h>
